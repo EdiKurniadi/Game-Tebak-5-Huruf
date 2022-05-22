12a1bb6847da4c8362a8fff7c9a7b464e77104f8
EdiKurniadi <82230087+EdiKurniadi@users.noreply.github.com>
2022-05-23 01:54:14 +0700

    Add files via upload
    update game

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 let words = ['abadi','abjad','abses','acara','acuan','aduan','afiks','agama','ageng','agung','ajaib','ajang','ajeng','akaid','akbar','akhir','aking','aksep','akses','aktif','aktor','akuan','al-ab','alami','alang','alari','allah','amang','ambah','ambar','amber','ambik','ambil','ambon','amino','amorf','ampai','ampat','amril','ancak','andai','andap','aneka','angan','angin','angit','angka','anglo','angsa','angus','anjak','antar','anyam','aorta','apkir','apnea','apung','arang','arkus','aroma','arsip','artis','arwah','askot','aspek','aster','atlet','audio','audit','auger','babak','babat','bacem','badai','badak','badal','badan','badar','badui','badut','bagai','bagan','bahan','bahar','bahas','baiti','bajak','bajul','bakal','bakar','bakas','bakat','bakau','bakda','bakti','bakul','balai','balam','balas','balig','balik','balok','balon','bambu','banar','banda','bando','bantu','bapak','barah','baran','barat','baris','barli','barut','basah','basal','batak','batal','batas','batik','batil','batin','batis','batuk','baung','bawah','bawal','bawat','bayam','bayan','bayar','bayur','beban','bebas','becak','bedah','bedak','bedil','bedol','behel','bekas','bekti','belah','belas','belek','belia','belit','belut','benam','benda','benih','benua','beras','berat','beres','berok','besan','besar','beser','besok','betis','beton','betul','biala','biang','biasa','biaya','bibir','bidai','bidal','bidan','bidik','biduk','bijak','bijih','bilih','bilik','biner','biola','biota','bisul','biuku','bobak','bobot','bodoh','bodot','bokor','boleh','bolon','bonet','bonus','boole','botol','brana','brata','braya','broca','buang','buaya','bubuk','bubul','bubur','bubus','bugil','buhul','bujuk','bujur','bukan','bukit','bukti','bulan','bular','bulat','buluh','bulus','bumbu','bunda','bunga','buntu','bunyi','burak','buram','buras','burat','bursa','buruh','buruk','busuk','busur','cabai','cacah','cacar','cacat','cadar','cadas','cagak','cagar','cakap','cakar','cakup','calir','calla','calon','caluk','campa','canai','canda','candi','candu','carik','carpe','carte','catur','causa','cawan','cecak','cegah','cekat','cekel','celah','celup','cemas','cemat','cepat','ceper','cerai','cerap','cerca','cerek','cerna','cetak','cetok','cheng','cikar','cilik','cinta','cipta','cirit','citra','civic','cloze','cobek','cocok','cogan','cokek','crash','cream','crede','cross','cuaca','cucut','cukai','cukup','cukur','culpa','cumbu','cupak','curah','dabus','dacin','dadak','dadap','dadar','dagun','dahak','dahan','dakwa','dalam','dalem','dalil','damai','damar','damas','danau','dandi','daong','dapat','dapur','darah','darat','dasar','dasun','datar','dawai','dayak','debit','debus','degan','dekat','delik','demam','denah','denak','denda','depan','depok','derap','deras','derau','derbi','derek','deret','detak','detus','dewan','dewar','didik','diksi','dikte','dinas','dirma','ditto','dolar','domba','dosen','dosis','dowel','drama','druwe','duduk','dukun','dumbo','dunia','duren','edema','edisi','ejaan','elang','embun','embus','emosi','empat','empuk','encer','encok','endap','entri','enzim','ereng','erosi','eskro','estat','etawa','faber','facto','faire','fajar','falak','fardu','femme','fenol','fiber','fikih','fiksi','finis','fisik','fitat','fluks','flynn','fobia','fokus','folat','folik','force','fotik','fovea','freud','froid','gabus','gadai','gadis','gaduh','gagah','gagak','gagal','gajah','galah','galat','galop','galur','gamat','gamit','ganas','ganda','ganti','gapai','garam','garis','garpu','garuk','garwa','gasal','gatal','gatra','gawai','gawat','gebel','gegak','gegap','gegar','gelak','gelam','gelap','gelar','gelas','gelut','geman','gempa','gemuk','genap','genta','gerai','gerak','geram','gerau','gerha','gesek','geser','geste','getah','getar','gigit','gilir','giral','gitar','gobar','gobek','godek','golek','goods','gosok','graha','grand','griya','gubal','gudeg','gugur','gugus','gulai','gurat','gurih','gurun','gusti','habis','hadat','hadis','hajat','hakim','hakul','halal','halus','halwa','hamba','hamil','hantu','hapus','haram','harap','harga','harta','hasil','hatta','haute','hayat','henle','henti','hepar','hewan','hibah','hidro','hidup','hijau','hilir','hindu','hisab','hitam','horor','hotel','hujan','hukum','humat','humus','huruf','hutan','ideal','ifrad','ihram','iklan','iklim','iloni','imbal','impor','inang','indra','induk','infra','ingat','inggu','ingin','injak','injil','insan','intai','intan','inter','ionik','irama','ireng','istri','jadah','jadid','jagat','jahat','jahil','jahit','jajan','jaket','jaksi','jalak','jalan','jalur','jambe','jambu','jamur','janda','janin','janji','janti','jarah','jarak','jaran','jarik','jarum','jatuh','jawab','jejak','jelak','jenis','jenuh','jepit','jepun','jerat','jerih','jeruk','jerum','jevon','jilid','jinem','jitah','jodoh','joglo','jotos','juang','jubah','judul','juita','jujur','jukut','jumpa','junta','jurai','kabar','kabel','kabin','kabir','kabul','kabur','kabut','kacau','kacer','kacip','kadas','kader','kadim','kadut','kafan','kafir','kaget','kahar','kakab','kakai','kakak','kakap','kakas','kalah','kalam','kalau','kalbu','kalis','kalor','kamar','kamus','kanal','kanan','kando','kanta','kapai','kapak','kapal','kapas','kapuk','kapur','karat','karet','kargo','karib','kartu','karun','karya','kasai','kasar','kasat','kaset','kasih','kasur','kasus','kasut','katai','katak','katik','katun','katup','kawak','kawal','kawan','kawat','kawin','kayan','kayuh','kebat','kebul','kebun','kecap','kecek','kecik','kecil','kecut','kedai','kedak','kejap','kejat','kekal','kekam','kekat','keket','kekok','kelam','kelas','kelat','kelim','kelip','kemas','kemih','kempa','kepal','kepik','kepok','kerah','kerak','keran','kerap','keras','kerek','kerik','keris','kerja','keruh','keruk','kerut','ketam','ketan','ketat','keter','ketua','ketuk','kibas','kidal','kidul','kijil','kikir','kilan','kilat','kilau','kilen','kilir','kimia','kipas','kipat','kiran','kirna','kista','kitab','klaim','klien','klise','kobok','kobra','kodal','kodok','kokas','kokat','koksi','kolam','kolar','kolek','kolom','kolot','kombo','komik','konon','kopak','koral','koran','koros','kotak','kotok','kotor','koyok','krama','krida','kriya','kuali','kuasa','kuasi','kubah','kubik','kubis','kubur','kubus','kuduk','kudus','kukus','kukut','kulat','kulit','kuman','kumba','kumuh','kumur','kunci','kunut','kupas','kupat','kurap','kuras','kurma','kursi','kurus','kurva','kusam','kusen','kusut','kutil','kutub','kutuk','kyrie','label','labuh','laesa','lagak','lahan','lahar','lahir','lajak','laken','lakon','lakur','lalai','lalat','lampu','lanar','lanny','lapak','lapis','larak','laras','larat','larik','laris','larva','lasak','laser','lasik','latar','laten','latih','latin','lawak','lawan','lawas','layah','layak','layar','layuh','lebah','lebam','lebar','lebih','lebur','ledak','legit','leher','lekas','lekat','lekuk','lelah','lelas','lemah','lemak','lemas','lembu','lemon','lensa','lepas','lerak','lerap','letak','leter','letih','lewat','liang','libur','licin','lidah','lihat','likur','lilin','lilit','liman','limau','limpa','lipan','lipas','lipat','lipit','liris','lisan','liung','liwet','lobus','locus','lodan','logam','lokal','lokus','lolos','loron','lotre','loyar','luban','lubuk','lukis','lulai','lumbu','lumut','lunak','lunas','lunau','lupuk','lupus','lurik','luruh','lurus','lusuh','lutut','maani','mabuk','macam','macan','madya','mafia','mahal','majal','majir','makam','makan','makna','makro','malai','malam','malas','malat','mamak','mandi','manik','manis','manja','manuk','mapan','marak','masak','masam','masih','masir','massa','masuk','mater','maung','mawai','mawar','mawas','mawon','mayat','mayor','mbaru','medan','media','medis','mekah','memar','meneg','meong','merah','merak','merek','merih','mesin','meter','mijen','mikro','milik','mimpi','minat','minor','minta','minum','mirat','misal','misan','mitra','mobil','modal','modem','modul','modus','momen','monde','moral','morse','mosso','motor','muara','mudah','mudik','mulas','mules','mulsa','mulut','murah','murai','murid','murka','murni','music','musik','musim','musuh','nafar','nafsu','nagan','nahar','nahas','nakal','nanah','nanas','nanti','napas','nasab','nasal','nasib','natal','navis','nazar','nenas','nenek','niaga','nifas','nikah','nilai','nilon','nisan','nobat','nobis','nomen','nomor','norma','novus','nujum','nuwun','nyata','nyawa','nyeri','nyiur','obeng','objek','obyok','oleat','ombak','opini','opium','optis','orang','organ','orkes','ovari','pacar','pacat','pacet','padan','padas','padat','padri','pagar','paham','pahat','pahit','pajak','pakai','paket','pakis','paksi','palak','palas','palat','palem','palma','palsu','pamor','panah','panas','panda','pandu','panel','panen','panji','panta','papan','parah','paras','paris','parsi','paruh','pasak','pasal','pasar','pasif','pasir','passu','pasta','pasti','patah','paten','patol','patri','pawai','payah','pecah','pecut','pedih','pegas','pegun','pekak','pekan','pekat','pelat','pelet','pelir','pelor','peluh','peluk','penak','penat','penuh','perah','perak','peran','perdu','pergi','peria','perlu','perum','perun','perut','pesak','pesan','pesat','peseh','pesta','petai','petak','petik','petir','petis','petus','piano','piara','piatu','pidie','pidit','pihak','pijar','pijat','piket','pikir','pilih','pilin','pinak','pintu','pipih','pipit','pirus','pisah','pisau','pitam','pitar','pivot','pohon','point','pojok','poket','pokok','polah','polis','polos','pompa','popok','popor','porak','porno','porok','poros','praja','premi','prima','primi','puasa','puber','pucat','pucuk','puisi','pujuk','pukal','pukat','pukul','pulas','pulau','pulpa','puluh','pulut','punar','punca','pupuh','pupuk','pupur','puput','purba','purwa','pusar','pusat','puseh','puspa','putar','putat','putih','putik','putra','putri','putus','puyuh','rabit','rabun','racun','radar','raden','radin','radio','ragam','ragil','rahat','rahim','rajah','rajet','rajut','raket','rakit','ralat','ramah','ramal','rambu','rames','ranah','ranau','rancu','rapat','rapel','rapor','rapuh','rasio','ratap','ratna','rawan','rawas','rawat','rawon','rayap','reban','rebut','receh','redup','rekor','reksa','renik','repuh','resah','resam','resep','reses','resmi','retak','rezim','riang','riben','riche','rimba','rindu','ripta','riset','roboh','rokan','roket','rokok','rolet','roman','rompi','rotan','rotor','ruang','rucah','rujak','rukun','rumah','rumus','rurut','rusak','rutin','sabak','saban','saben','sabit','sabuk','sabun','sabut','sadap','safar','safir','sagir','saham','saing','sajak','sakal','sakat','sakit','saksi','sakti','salah','salai','salak','salam','salat','saldo','saleh','salik','salju','salto','samak','samar','samin','sanak','sanca','sanda','sandi','sando','sanga','saraf','saran','sarat','saron','sasar','satai','satwa','sawah','sawan','sawar','sawat','sayap','sayur','sebab','sebar','sedan','sedap','sedia','segar','sehat','seibu','sejuk','sekam','sekar','sekat','selam','selir','selop','selot','semah','semak','semar','semat','semen','semir','semut','senam','sende','sendi','senja','sepah','sepak','sepan','sepat','sepit','sepoi','sepuh','serap','serat','serau','serbu','serta','serum','sesai','sesar','sesat','setan','setia','setik','setum','siaga','siang','siapa','siber','sibur','sidik','sifat','sihir','sikap','sikat','silap','silat','silih','silvi','sinar','sinau','singa','sinse','sinus','sipat','sipil','sipit','siput','sirai','siram','sirih','sirik','sirip','sirop','sirus','sisik','sisir','siswa','sitae','skala','skrip','sluri','sodok','sogok','solar','sonde','sonok','sorak','sorot','sotto','souci','spion','spons','sport','stres','strok','studi','suaka','suara','suasa','subak','subuh','subur','sudah','sudip','sudut','sufah','sujan','sukar','sukat','sulah','sulam','suluh','suluk','sumbi','sumbu','summa','sumur','sunah','sunda','sungu','sunti','super','supra','suram','surat','surut','surya','susah','susuk','susun','susur','sutra','syura','tabah','tabel','tabib','tabii','tabir','tadah','tahan','tahap','tahun','tajam','tajuk','takat','takik','taksi','takwa','talak','talam','talas','taman','tamat','tanah','tanak','tanam','tanat','tanda','tandu','tanpa','tanur','tanya','tapak','tapal','taram','tarek','tarif','tarik','taruh','tarum','tatal','tatap','tawaf','tawar','tebal','teban','tebar','tegak','tegap','tegar','tegur','tekak','tekan','tekat','teken','tekuk','telon','teluk','telum','telur','teman','tempa','tempe','tempo','tenda','tenis','tenun','teori','tepat','tepok','tepuk','terap','teras','teror','terup','terus','tetap','tetas','tetek','tetes','tetra','tiang','tidak','tidur','tikam','tikar','tiket','tikus','tilam','tilas','timah','timun','timur','tinea','tinju','tinta','tipis','tirai','tirus','titen','titik','titip','tobat','tohok','tokol','tolak','tolok','tomat','tonil','topan','topas','torak','total','triko','troya','tuang','tubuh','tuduh','tufah','tugas','tujuh','tukak','tukar','tukul','tulah','tulen','tulis','tunai','tunas','tunda','tunik','tupai','turis','turun','tusuk','tutul','tutup','tutur','udang','udara','udeng','ujian','ujung','ulama','ulang','umban','umbuk','umker','umpak','umpan','undak','undur','union','unjuk','unsur','unyil','upaya','urang','urban','urung','urusy','usaha','utama','utang','utara','uwita','verba','versa','vinil','virus','vitae','vital','vivre','vokal','wadak','wagon','wajah','wajar','wajib','wakil','waktu','wangi','warga','waris','warna','warta','wasit','watak','wawas','wayah','wesel','wetan','wiron','wisma','wrase','yaman','yatim','yesus','yudha','zadah','zakar','zakat','zaman','zarah','zirah']
 
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+};
 
 let randomNUmber = Math.floor(Math.random()*1675);
 const secretWord = words[ randomNUmber ];
@@ -10,33 +24,55 @@ let tryWords = [];
 let containerWords = document.getElementById('container-words')
 let allWords = document.querySelectorAll('.word');
 
-let guestWord = document.getElementById('guest');
 let numberGuest = 0;
+let guestWord = document.getElementById('guest');
+let answer = document.getElementById('answer');
 let enterButton = document.getElementById('enter-button');
+
+let winSound = new sound('audio/fairy-win.wav');
+let unlockSound = new sound('audio/unlock-game-notification.wav');
+let loseSound = new sound('audio/losing-drums.wav')
 
 let minute = parseInt(document.getElementById('minute').innerHTML);
 let second = parseInt(document.getElementById('second').innerHTML);
 
-let timer = setInterval(function() {
-	if( minute !== 0 && second === 0 ) {
-		minute--;
-		second = 59; 
-		document.getElementById('minute').innerHTML = minute;
-		document.getElementById('second').innerHTML = second;
-	} else if(minute >= 0  && second !== 0 ) {
-		second--;
-		document.getElementById('minute').innerHTML = minute;
-		document.getElementById('second').innerHTML = second;
-	} else {
-		clearInterval(timer);
-		gameFinish = true;
-		enterButton.innerHTML = 'Mulai Lagi';
-		guestWord.setAttribute('style',"display:none;");
-		// window.alert('Sayang sekali waktu sudah habis');
-		swal({title:'sayang sekali waktu sudah habis'});
-		return;
-	}
-}, 1000);
+let timer;
+function startTimer(){
+	timer = setInterval(function() {
+		if( minute !== 0 && second === 0 ) {
+			minute--;
+			second = 59; 
+			document.getElementById('minute').innerHTML = minute;
+			document.getElementById('second').innerHTML = second;
+		} else if(minute >= 0  && second !== 0 ) {
+			second--;
+			document.getElementById('minute').innerHTML = minute;
+			document.getElementById('second').innerHTML = second;
+		} else {
+			clearInterval(timer);
+			gameFinish = true;
+			enterButton.innerHTML = 'Mulai Lagi';
+			guestWord.setAttribute('style',"display:none;");
+			answer.innerHTML = `kata yang dimaksud adalah ${secretWord.toUpperCase()}`;
+			answer.setAttribute('style','display:block;');
+			// window.alert('Sayang sekali waktu sudah habis');
+			loseSound.play();
+			swal({title:'sayang sekali waktu sudah habis'});
+			return;
+		}
+	}, 1000);
+}; 
+
+let pointElem = document.getElementById('point');
+let point = 0;
+let markTimeMinute = minute;
+let markTimeSecond = second;
+let markIndexSecretWordGreen = [1,1,1,1,1];
+let markIndexSecretWordOrange = [1,1,1,1,1];
+function beautyPoint(point) {
+	return Math.floor((Math.floor(point)/1650)*1000);
+}
+
 
 enterButton.addEventListener('click', function(ev) {
 	ev.preventDefault();
@@ -48,7 +84,7 @@ enterButton.addEventListener('click', function(ev) {
 
 	if(guestWord.value.toLowerCase().length !== 5 ) {
 		// window.alert('tebakan harus terdiri dari 5 angka');
-		swal({title:"tebakan harus terdiri dari 5 angka"});
+		swal({title:"tebakan harus terdiri dari 5 huruf"});
 		return;
 	};
 
@@ -67,13 +103,32 @@ enterButton.addEventListener('click', function(ev) {
 		allWords[allWords.length-1].children[i].innerHTML = guestWord.value.toLowerCase().charAt(i).toUpperCase();
 		if( guestWord.value.toLowerCase().charAt(i) === secretWord.charAt(i) ) {
 			allWords[allWords.length-1].children[i].classList.add('green');
+			unlockSound.play();
+			
+			point += 20/markIndexSecretWordGreen[i];
+			let timeLapse = (markTimeMinute - minute)*60 + (markTimeSecond- second);
+			point += timeLapse < 60 ? Math.floor((60-timeLapse)/6) : 0;
+			markIndexSecretWordGreen[i]++;
+
+
 		} else if ( secretWordArray.indexOf(guestWord.value.toLowerCase().charAt(i)) !== -1 ) {
 			allWords[allWords.length-1].children[i].classList.add('orange');
+			
+			point += 4/markIndexSecretWordOrange[i];
+			let timeLapse = (markTimeMinute - minute)*60 + (markTimeSecond- second);
+			point += timeLapse < 60 ? Math.floor((60-timeLapse)/20) : 0;
+			markIndexSecretWordOrange[i]++;
+
 		};
 	};
 
+	pointElem.innerHTML = `poin: ${beautyPoint(point)}`;
+
 	if(guestWord.value.toLowerCase() === secretWord) {
 		clearInterval(timer);
+		winSound.play();
+		point += 15*(10-numberGuest)*(10-numberGuest);
+		pointElem.innerHTML = `poin: ${beautyPoint(point)}`;
 		// window.alert('selamat kamu BERHASIL');
 		swal({
 		  title: "Selamat Kamu BERHASIL!",
@@ -88,15 +143,23 @@ enterButton.addEventListener('click', function(ev) {
 	}
 
 	numberGuest++;
+	if(numberGuest === 1 ) {
+		startTimer();
+	};
+
 	if(numberGuest === 6) {
 		clearInterval(timer);
 		gameFinish = true;
 		enterButton.innerHTML = 'Mulai Lagi';
 		guestWord.setAttribute('style',"display:none;");
+		answer.innerHTML = `kata yang dimaksud adalah ${secretWord.toUpperCase()}`;
+		answer.setAttribute('style','display:block;');
+		loseSound.play();
 		// window.alert('Sayang sekali kesempatanmu sudah habis');
 		swal({title:"Sayang sekali kesempatanmu sudah habis"});
+
 		return;
-	}
+	};
 
 	let newGuest = document.createElement('div');
 	newGuest.classList.add('word');
@@ -110,5 +173,8 @@ enterButton.addEventListener('click', function(ev) {
 	allWords = document.querySelectorAll('.word');
 	tryWords.push(guestWord.value.toLowerCase());
 	guestWord.value = '';
+
+	markTimeMinute = minute;
+	markTimeSecond = second;
 
 })
